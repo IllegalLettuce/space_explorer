40baf96e780873d447869856b05bc86def2dfdf5
Anthony Sabbe <100137579+IllegalLettuce@users.noreply.github.com>
2024-04-09 20:03:07 +0100

    Comedic amount of RAM usage

--- space_solution.c
+++ space_solution.c
@@ -7,7 +7,7 @@
 struct ship_state_struct {
     int number_of_planets_visited; //number of overall visited planets
     unsigned int *planets_seen; //Pointer to array of observed planets
-    int number_of_planets_seen;
+    int number_of_planets_seen; //number of seen planets
     unsigned int *planets_visited; //Pointer to array of visited planets
 };
 
@@ -28,15 +28,19 @@ ShipAction space_hop(unsigned int crt_planet, //Current planet
         struct ship_state_struct *state = malloc(sizeof(struct ship_state_struct));
 
         //Have not visited any planets yet
-        state->number_of_planets_visited = 0;
 
-        //All seen planets
+
+        //All seen planets.
+        //Increases the amount of seen planets and adds each pointer to the array
         state->number_of_planets_seen = num_connections;
-        state->planets_seen = malloc((num_connections + 1)*sizeof(unsigned int));
-        state->planets_seen[0] = *connections;
+        state->planets_seen = malloc((num_connections)*sizeof(unsigned int));
+        for (int index = (state->number_of_planets_seen-num_connections);index<state->number_of_planets_seen;index++){
+            state->planets_seen[index] = connections[index];
+        }
 
         //All visited planets
-        state->planets_visited = malloc((state->number_of_planets_visited + 1)* sizeof(unsigned int));
+        state->number_of_planets_visited = 1;
+        state->planets_visited = malloc((state->number_of_planets_visited)* sizeof(unsigned int));
         state->planets_visited[0] = crt_planet;
 
         printf("Number of planets visited: %d\n", state->number_of_planets_visited);
@@ -49,31 +53,48 @@ ShipAction space_hop(unsigned int crt_planet, //Current planet
     //Refresh database
     struct ship_state_struct *state = ship_state;
 
-    //Update number of planets visited;
-    state->number_of_planets_visited += 1;
-    printf("Number of planets visited: %d\n",state->number_of_planets_visited);
+    //The planets_seen
+    int number_new_seen = 0;
+    for (int index = 0; index < (state->number_of_planets_seen + num_connections); index++){
+        int bool_already_seen = 0;
 
-    //Resize the planets_seen
+        for (int inner_index = 0; inner_index < num_connections; inner_index++){
+
+            if (state->planets_seen[index] == connections[inner_index]){
+                bool_already_seen = 1;
+                break;
+            }
+        }
+        if (!bool_already_seen){
+            number_new_seen++;
+        }
+    }
+
+
+    state->number_of_planets_seen += number_new_seen;
     unsigned int *planets_temp_ptr;
     planets_temp_ptr = realloc(state->planets_seen,
-                               (state->number_of_planets_seen+num_connections) * sizeof(unsigned int));
+                               (state->number_of_planets_seen) * sizeof(unsigned int));
+    printf("Check\n");
     if (planets_temp_ptr != NULL){
         state->planets_seen = planets_temp_ptr;
-        free(planets_temp_ptr);
     }else{
         printf("Something went wrong with the realloc for planets_seen;\n");
     }
 
-    //Resize the planets_visited
+    //The planets_visited
+    state->number_of_planets_visited += 1;
+    printf("Number of planets visited: %d\n",state->number_of_planets_visited);
+
     unsigned int *planets_visited_temp;
     planets_visited_temp = realloc(state->planets_visited,
-                                   (state->number_of_planets_visited+1) * sizeof(unsigned int));
+                                   (state->number_of_planets_visited) * sizeof(unsigned int));
     if (planets_visited_temp != NULL){
         state->planets_visited = planets_visited_temp;
-        free(planets_visited_temp);
     }else{
         printf("Something went wrong with the realloc for planets_visited;\n");
     }
+
 
 
 
