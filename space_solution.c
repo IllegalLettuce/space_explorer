b39443f4d9b40c0ec2d9fbeb26d36cbfc3ce724b
Anthony Sabbe <100137579+IllegalLettuce@users.noreply.github.com>
2024-04-10 09:37:42 +0100

    Random jump attempt, but has memory leak and 0xC0000374 error. Suspect its gotta do with the malloc or the decrementing

--- space_solution.c
+++ space_solution.c
@@ -2,20 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#define RANDOM_JUMP_AMOUNT 5
+
+struct planet_storage {
+
+};
 
 //Ship database. A structure that hold all the information
 struct ship_state_struct {
-    int number_of_planets_visited; //number of overall visited planets
-    unsigned int *planets_seen; //Pointer to array of observed planets
-    int number_of_planets_seen; //number of seen planets
-    unsigned int *planets_visited; //Pointer to array of visited planets
+    int number_of_planets_visited;                  //number of overall visited planets
+    unsigned int *planets_visited;                  //Pointer to array of visited planets
+    int first_random_jumps_amount;                  //Amount of random jumps storage (this number is decremented during the sweep)
+    int num_connections_count;                      //Number of connections in current sweep
+    unsigned int *connections_data;                 //Data of the neighbouring connections
+    double *distance_of_connections_array;          //Data of the distance to the mixer for each sweep
 };
 
-ShipAction space_hop(unsigned int crt_planet, //Current planet
-                   unsigned int *connections, //Pointer to array of integers. ID of planets you can go to
-                   int num_connections, //number of planets in array - number of neighbour planets
-                   double distance_from_mixer, //crows flies distance to target planet
-                   void *ship_state) //pointer to storage structure
+ShipAction space_hop(unsigned int crt_planet,       //Current planet
+                   unsigned int *connections,       //Pointer to array of integers. ID of planets you can go to
+                   int num_connections,             //number of planets in array - number of neighbour planets
+                   double distance_from_mixer,      //crows flies distance to target planet
+                   void *ship_state)                //pointer to storage structure
 {
     printf("==============================================\n");
     printf("Distance from mixer: %f\n", distance_from_mixer);
@@ -26,19 +33,9 @@ ShipAction space_hop(unsigned int crt_planet, //Current planet
     if (ship_state == NULL){
         printf("Ship state is null\n");
         struct ship_state_struct *state = malloc(sizeof(struct ship_state_struct));
-
-        //Have not visited any planets yet
-
-
-        //All seen planets.
-        //Increases the amount of seen planets and adds each pointer to the array
-        state->number_of_planets_seen = num_connections;
-        state->planets_seen = malloc((num_connections)*sizeof(unsigned int));
-        for (int index = (state->number_of_planets_seen-num_connections);index<state->number_of_planets_seen;index++){
-            state->planets_seen[index] = connections[index];
-        }
-
-        //All visited planets
+        state->first_random_jumps_amount = RANDOM_JUMP_AMOUNT;
+        state->connections_data = malloc(RANDOM_JUMP_AMOUNT * sizeof(unsigned int));
+        state->num_connections_count = RANDOM_JUMP_AMOUNT;
         state->number_of_planets_visited = 1;
         state->planets_visited = malloc((state->number_of_planets_visited)* sizeof(unsigned int));
         state->planets_visited[0] = crt_planet;
@@ -46,46 +43,14 @@ ShipAction space_hop(unsigned int crt_planet, //Current planet
         printf("Number of planets visited: %d\n", state->number_of_planets_visited);
         struct ship_action next_action = {RAND_PLANET, state};
         return next_action;
-    }
-    //Next planet to jump to
-    unsigned int_next_planet = RAND_PLANET;
 
+    }
     //Refresh database
     struct ship_state_struct *state = ship_state;
-
-    //The planets_seen
-    int number_new_seen = 0;
-    for (int index = 0; index < (state->number_of_planets_seen + num_connections); index++){
-        int bool_already_seen = 0;
-
-        for (int inner_index = 0; inner_index < num_connections; inner_index++){
-
-            if (state->planets_seen[index] == connections[inner_index]){
-                bool_already_seen = 1;
-                break;
-            }
-        }
-        if (!bool_already_seen){
-            number_new_seen++;
-        }
-    }
-
-
-    state->number_of_planets_seen += number_new_seen;
-    unsigned int *planets_temp_ptr;
-    planets_temp_ptr = realloc(state->planets_seen,
-                               (state->number_of_planets_seen) * sizeof(unsigned int));
-    printf("Check\n");
-    if (planets_temp_ptr != NULL){
-        state->planets_seen = planets_temp_ptr;
-    }else{
-        printf("Something went wrong with the realloc for planets_seen;\n");
-    }
 
     //The planets_visited
     state->number_of_planets_visited += 1;
     printf("Number of planets visited: %d\n",state->number_of_planets_visited);
-
     unsigned int *planets_visited_temp;
     planets_visited_temp = realloc(state->planets_visited,
                                    (state->number_of_planets_visited) * sizeof(unsigned int));
@@ -96,6 +61,47 @@ ShipAction space_hop(unsigned int crt_planet, //Current planet
     }
 
 
+    unsigned int next_planet;
+
+    //Random jump logic
+    if (state->first_random_jumps_amount > -1){
+        if (state->first_random_jumps_amount == RANDOM_JUMP_AMOUNT){
+            state->distance_of_connections_array = malloc(RANDOM_JUMP_AMOUNT * sizeof(unsigned int));
+        }
+        state->distance_of_connections_array[RANDOM_JUMP_AMOUNT-(state->first_random_jumps_amount)]=distance_from_mixer;
+
+        state->connections_data[RANDOM_JUMP_AMOUNT-(state->first_random_jumps_amount)]=crt_planet;
+        printf("%d\n", state->first_random_jumps_amount);
+        state->first_random_jumps_amount--;
+        printf("RANDOM JUMP %d\n", state->first_random_jumps_amount);
+        struct ship_action next_action = {RAND_PLANET, state};
+        return next_action;
+    }
+    printf("Check\n");
+    double lowest_distance = 10.5;
+    int index_of_lowest_distance = 0;
+
+    for (int index = 0; index < state->num_connections_count; index++){
+        if (state->distance_of_connections_array[index] > lowest_distance){
+            lowest_distance = state->distance_of_connections_array[index];
+            index_of_lowest_distance = index;
+        }
+    }
+
+    next_planet = state->connections_data[index_of_lowest_distance];
+
+
+
+
+    //Dollar store Dijkstra's algo
+    //From each planet, save connections and num_connections check
+    //Substructure????
+    //N random jumps (optimise)
+    //Jump through each connection in order and save distances to array
+    //index of array of distances correspond to array of connections
+    //find the shortest distance
+    //jump to planet
+    //repeat
 
 
 
@@ -104,6 +110,6 @@ ShipAction space_hop(unsigned int crt_planet, //Current planet
 
 
     //Next Jump
-    struct ship_action next_action = {int_next_planet, state};
+    struct ship_action next_action = {next_planet, state};
     return next_action;
 }
